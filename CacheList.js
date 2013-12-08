c871500f03e541b4b76b371fa6862bbda1a2781b
Tom Ashworth <tashworth@twitter.com>
2013-12-08 21:12:39 +0000

    CacheList, ready and methods.

--- CacheList.js
+++ CacheList.js
@@ -1,14 +1,74 @@
 var util = require('util');
-util.inherits(CacheList, Array);
+var hide = require('./hide');
+var instanceOf = require('./instanceOf');
+var Cache = require('./Cache');
+var Promise = require('promise');
 
 module.exports = CacheList;
 
 function CacheList() {
-    Array.apply(this, arguments);
+    hide(this, '_cacheIndexMap', {});
+    hide(this, '_cacheList', []);
 }
+
+CacheList.prototype.ready = function () {
+    return Promise.all(this._cacheList.map(function (cache) {
+        return cache.ready();
+    }));
+};
 
 CacheList.prototype.match = function () {};
 
-CacheList.prototype.get = function (key) {};
+CacheList.prototype.get = function (key) {
+    return this._cacheList[this._cacheIndexMap[key]];
+};
 
-CacheList.prototype.set = function (key, value) {};
+CacheList.prototype.set = function (key, value) {
+    if (!instanceOf(value, Cache)) {
+        throw new TypeError('CacheList value must be a Cache.');
+    }
+    var len = this._cacheList.push(value);
+    this._cacheIndexMap[key] = len - 1;
+};
+
+CacheList.prototype.forEach = function (callback, ctx) {
+    var cacheMap = this.keys.reduce(function (memo, key) {
+        var index = this._cacheIndexMap[key];
+        memo[key] = this._cacheList[index];
+        return memo;
+    }.bind(this), {});
+
+    return this.keys.forEach(function (key) {
+        var index = this._cacheIndexMap[key];
+        var cache = this._cacheList[index];
+        return callback.call(ctx || cache, cache, key, cacheMap);
+    }.bind(this));
+};
+
+Object.defineProperty(CacheList.prototype, 'items', {
+    get: function () {
+        return this.values;
+    },
+    enumberable: true
+});
+
+Object.defineProperty(CacheList.prototype, 'keys', {
+    get: function () {
+        return Object.keys(this._cacheIndexMap);
+    },
+    enumberable: true
+});
+
+Object.defineProperty(CacheList.prototype, 'values', {
+    get: function () {
+        return [].slice.call(this._cacheList);
+    },
+    enumberable: true
+});
+
+Object.defineProperty(CacheList.prototype, 'size', {
+    get: function () {
+        return this._cacheList.length;
+    },
+    enumberable: true
+});
--- worker.js
+++ worker.js
@@ -1,17 +1,26 @@
 this.version = 2;
 
+var caches = this.caches;
 this.addEventListener('install', function (e) {
   e.services = ['fetch'];
 
   // Create a new cache and register it with the name 'app-1'
   // These resources are immediately cached
-  var cache = new Cache('https://api.github.com/users/phuu');
-  // this.caches.set('app-2', cache);
+  var cache = new Cache('app.2.css');
+  caches.set('app-2', cache);
+
+  console.log('== foreaching ========================');
+  caches.forEach(console.log.bind(console));
+  console.log('== done ========================');
+  console.log('caches.keys', caches.keys);
+  console.log('caches.items', caches.items);
+  console.log('caches.values', caches.values);
+  console.log('caches.size', caches.size);
 
   // Delay registration until the cache(s) are filled
-  cache.ready().then(function () {
-    console.log('Cache ready!');
-    console.log('cache', cache);
+  caches.ready().then(function () {
+    console.log('Caches ready!');
+    console.log('caches', caches);
   }, function (why) {
     console.log('Caching failed', why);
   });
