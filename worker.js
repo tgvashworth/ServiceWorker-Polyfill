cd5917543dd697227de4ac3faf4b7ed3859ece65
Tom Ashworth <tashworth@twitter.com>
2013-12-11 20:03:00 +0000

    Cache hit/miss in worker.

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ var caches = this.caches;
 this.addEventListener('install', function (e) {
     e.services = ['fetch'];
     var cache = new Cache('/assets/shell.3.html', '/assets/app.3.css');
-    caches.set('app-4', cache);
+    caches.set('app', cache);
     e.waitUntil(caches.ready()).then(function () {
         console.log('ServiceWorker ready.');
     });
@@ -13,13 +13,19 @@ this.addEventListener('install', function (e) {
 this.addEventListener('fetch', function (event) {
     console.log(event.type, event.request.url);
     // Redirect all navigations to the shell page, which can figure out the route
-    if (event.type === 'navigate' && event.isTopLevel) {
+    if (event.type === "navigate" && event.isTopLevel) {
         return event.respondWith(
             caches.match('/assets/shell.3.html')
         );
     }
     // Try to grab the thing from the cache
     event.respondWith(
-        caches.match(event.request.url)
+        caches.match(event.request.url).then(function (response) {
+            console.log('cache hit', event.request.url, response);
+            return response;
+        }, function (why) {
+            console.log('cache miss', event.request.url);
+            throw why;
+        })
     );
 });
