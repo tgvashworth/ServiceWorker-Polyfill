0cdffdd98f65376b7440d673d66467b79c2c3aaa
Tom Ashworth <tashworth@twitter.com>
2014-01-05 15:57:15 +0000

    Add message event listening to the worker.

--- worker.js
+++ worker.js
@@ -8,3 +8,7 @@ this.addEventListener('install', function (event) {});
 this.addEventListener('fetch', function (event) {
     console.log('event.request.url', event.request.url);
 });
+
+this.addEventListener('message', function (event) {
+    console.log('message event', event);
+});
