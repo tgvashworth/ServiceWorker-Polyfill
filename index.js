2485db3e06bcd5edb60a993f8bfcce87086f4e39
Jake Archibald <jaffathecake@gmail.com>
2014-01-08 17:45:31 +0000

    Adding options --no-browser Don't start Chrome --browser-only Only start Chrome --browser [path] Custom path to browser

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
+var Promise = require('rsvp').Promise;
 var browserLauncher = require('./browserLauncher');
 var startServer = require('./server').startServer;
 var argv = require('optimist').argv;
 
+
 var proxyPort = argv.port || 5678;
 var workerPath = argv.worker || "worker.js";
+var browser = argv.browser;
+var browserOnly = argv['browser-only'];
 
-browserLauncher(argv['browser-path'], proxyPort).then(function(browserProcess) {
-  browserProcess.on('exit', function(code) {
-    if (code) {
-      console.error("Browser unexpectedly exited");
-      process.exit(1);
+Promise.resolve().then(function() {
+    if (browser === false) { // as in --no-browser
+        return;
     }
-    else {
-      process.exit(0);
-    }
-  });
+
+    return browserLauncher(browser, proxyPort).then(function(browserProcess) {
+        browserProcess.on('exit', function(code) {
+            if (code) {
+                console.error("Browser unexpectedly exited");
+                process.exit(1);
+            }
+            else {
+                process.exit(0);
+            }
+        });
+    });
 }).catch(function(err) {
     console.error(err.message);
     process.exit(1);
 }).then(function() {
+    if (browserOnly) { // as in --browser-only
+        return;
+    }
     startServer(proxyPort, workerPath);
 });
