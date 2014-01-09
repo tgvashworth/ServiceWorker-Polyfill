a5b94d8885e20873cfd62f8afaf8ead421640c75
Andy Hume <ahume@twitter.com>
2014-01-09 22:15:24 +0000

    Throw error if any request in importScripts() fail.

--- spec/importScripts.js
+++ spec/importScripts.js
@@ -1,9 +1,10 @@
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var URL = require('dom-urls');
 
 module.exports = function () {
 
 	var disabled = false;
-	var importer = function importer() {
+	var importer = function importer(workerUrl) {
 
 		if (disabled) {
 			throw new Error('You cannot call importScripts outside of initial setup.');
@@ -16,15 +17,17 @@ module.exports = function () {
 		// Sync get each URL and return as one string to be eval'd.
 		// These requests are done in series. TODO: Possibly solve?
 		return urls.map(function(url) {
+			var absoluteUrl = new URL(url, workerUrl);
 			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url, false);
+			xhr.open('GET', absoluteUrl, false);
 			xhr.send(null);
 			xhr.status = parseInt(xhr.status, 10);
 			if (xhr.status === 200) {
 				return xhr.responseText;
 			} else {
-				console.log('importScripts error. Status', xhr.status);
-				return ''; // Return nothing if error.
+				console.log('Status:', xhr.status);
+				console.log('URL:', absoluteUrl.toString());
+				throw new Error('Network error while calling importScripts()');
 			}
 		}).join('\n;\n');
 	}
