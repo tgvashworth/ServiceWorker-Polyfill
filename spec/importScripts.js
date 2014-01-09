b7090fc8acba8f1b902db9130794fb1cc7b6c330
Andy Hume <ahume@twitter.com>
2014-01-09 17:02:57 +0000

    Let importScripts support multiple URLs as args.

--- spec/importScripts.js
+++ spec/importScripts.js
@@ -4,7 +4,7 @@ module.exports = function () {
 
 	var disabled = false;
 
-	var importer = function (url) {
+	var importer = function () {
 
 		importer.disable = function() {
 			disabled = true;
@@ -15,12 +15,23 @@ module.exports = function () {
 			throw 'You cannot call importScripts outside of initial setup.';
 		}
 
-		console.log('called importScripts with url:', url);
-		// Sync get URL and execute in the worker context.
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url, false);
-		var r = xhr.send(null);
-		return xhr.responseText;
+		var urls = Array.prototype.slice.call(arguments, 0).filter(function(arg) {
+			return typeof arg === 'string';
+		});
+
+		// Sync get each URL and return as one string to be eval'd.
+		// This requests are done in series. TODO: Possibly solve?
+		return urls.map(function(url) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url, false);
+			xhr.send(null);
+			if(xhr.status === '200') {
+				return xhr.responseText;
+			} else {
+				console.log('importScripts error. Status', xhr.status);
+				return ''; // Return nothing if error.
+			}
+		}).join(';');
 	}
 
 	return importer;
